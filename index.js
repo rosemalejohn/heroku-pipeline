baab4bf6d0d90db910f1eda47d892ed334ec11c8
Rosemale-John II Villacorta <rosemalejohn@gmail.com>
2018-05-04 23:10:51 +0800

    Initialize graphql

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const Koa = require('koa');
+const Router = require('koa-router');
+const Body = require('koa-bodyparser');
+const { graphqlKoa, graphiqlKoa } = require('apollo-server-koa');
 const app = new Koa();
 
-app.use(async ctx => {
-  ctx.body = 'Hello world!';
-});
+Router.post('/graphql', Body(), graphqlKoa({ schema: myGraphQLSchema }));
+Router.get('/graphql', graphqlKoa({ schema: myGraphQLSchema }));
 
+Router.get('/graphiql', graphiqlKoa({ endpointURL: '/graphql' }));
+
+app.use(Router.routes());
+app.use(Router.allowedMethods());
 app.listen(3000);
